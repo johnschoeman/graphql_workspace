413faebd1e8b7601973d470c65e16cffc0986bcf
John Schoeman <johnschoeman1617@gmail.com>
2017-10-09 10:29:12 -0700

    Add auth to hacker-news

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { gql, graphql } from 'react-apollo';
+import { GC_USER_ID } from '../constants'
 
 class CreateLink extends Component {
 
@@ -38,26 +39,38 @@ class CreateLink extends Component {
   }
 
   _createLink = async () => {
+    const postedById = localStorage.getItem(GC_USER_ID)
+    if (!postedById) {
+      console.error('No user logged in')
+      return
+    }
     const { description, url } = this.state
     await this.props.createLinkMutation({
       variables: {
         description,
-        url
+        url,
+        postedById
       }
     })
+    this.props.history.push(`/`)
   }
 }
 
 const CREATE_LINK_MUTATION = gql`
-  mutation CreateLinkMutation($description: String!, $url: String!) {
+  mutation CreateLinkMutation($description: String!, $url: String!, $postedById: ID!) {
     createLink(
       description: $description,
       url: $url,
+      postedById: $postedById
     ) {
       id
       createdAt
       url
       description
+      postedBy {
+        id
+        name
+      }
     }
   }
 `
