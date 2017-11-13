51074c5c6d299f235550649f86931c3835c4a9c6
John Schoeman <johnschoeman1617@gmail.com>
2017-11-12 22:18:41 -0800

    Finish twitter-clone part 3

---
+++ equimper-twitter-clone/server/src/graphql/resolvers/user-resolvers.js
@@ -0,0 +1,28 @@
+import User from '../../models/User';
+
+export default {
+  signup: (_, { fullName, username, password, email, avatar }) => {
+    const [firstName, ...lastName] = fullName.split(' ');
+    return User.create({
+      firstName,
+      lastName,
+      username,
+      password,
+      email,
+      avatar,
+    });
+  },
+  login: async (_, { email, password }) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error('User not exist!');
+    }
+
+    if (!user.authenticateUser(password)) {
+      throw new Error('Password not match!');
+    }
+
+    return user;
+  },
+};
--- equimper-twitter-clone/server/src/graphql/schema.js
+++ equimper-twitter-clone/server/src/graphql/schema.js
@@ -1,5 +1,15 @@
 export default `
 scalar Date
+type User {
+  _id: ID!
+  username: String
+  email: String!
+  firstName: String
+  lastName: String
+  avatar: String
+  createdAt: Date!
+  updatedAt: Date!
+}
 type Tweet {
   _id: ID!
   text: String!
@@ -17,6 +27,8 @@ type Mutation {
   createTweet(text: String!): Tweet
   updateTweet(_id: ID!, text: String!): Tweet
   deleteTweet(_id: ID!): Status
+  signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): User
+  login(email: String!, password: String!): User
 }
 schema {
   query: Query
